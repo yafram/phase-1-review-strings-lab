26adf788d0daac49b0b26ab8f01872a8175aef12
Jacqueline Watson-Sinclair <faycello75@gmail.com>
2023-03-29 03:45:24 -0400

    let keyword defined variables

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+let currentUser = "Jake Smith";
+
+let welcomeMessage = "Welcome to Flatbook, Jake Smith!";
+
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, JAKE SMITH!";
+
+let shortGreeting = "Welcome, J!"
+
+
